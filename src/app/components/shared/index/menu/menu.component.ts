611718467a398e5653454a8243ec4c51f758bbef
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-03-22 17:07:05 -0500

    Iteraccion del carrito, mejorando responsive
    Se agrego la iteraccion de carrito de compra con eventos de botones de subir y bajar cantidad de dicho producto,  se mejoro el responsive de la pagina index

--- src/app/components/shared/index/menu/menu.component.ts
+++ src/app/components/shared/index/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UrlFront } from '../../routes/RoutesFront';
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +8,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-  constructor(private router:Router) {}
+  ///CATEGORIAs ITEmS
+  categoriaItems: any[] = [
+    {
+      id: 1,
+      name: 'Cosméticos y bellezas',
+    },
+    {
+      id: 2,
+      name: 'Estilos y moda',
+    },
+    {
+      id: 3,
+      name: 'Deportes y actividades',
+    },
+
+    {
+      id: 4,
+      name: 'Tecnología e innovación',
+    },
+
+    {
+      id: 5,
+      name: 'Hogar y decoración',
+    },
+
+    {
+      id: 6,
+      name: 'Electrodomésticos',
+    },
+    {
+      id: 7,
+      name: 'Juguetería',
+    },
+    {
+      id: 8,
+      name: 'Salud y cuidado personal',
+    },
+    {
+      id: 9,
+      name: 'Joyería y accesorios',
+    },
+  ];
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
-
+  menuIndex() {
+    this.router.navigateByUrl(`${UrlFront.Menu}/${UrlFront.Menu.index}`);
+  }
 }
