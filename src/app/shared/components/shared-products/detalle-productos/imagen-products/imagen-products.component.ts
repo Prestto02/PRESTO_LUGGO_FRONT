ebc8bd0222ce413eafcf58bec13ae91f14b42e00
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-04-28 15:08:23 -0500

    Documentando codigo
    Se documento el codigo de las imagenes

--- src/app/shared/components/shared-products/detalle-productos/imagen-products/imagen-products.component.ts
+++ src/app/shared/components/shared-products/detalle-productos/imagen-products/imagen-products.component.ts
@@ -24,31 +24,33 @@ export class ImagenProductsComponent implements OnInit {
   ngOnInit(): void {
     this.obtenerElarrayDeimagenes();
   }
+  //OBTENER EL ARRAY DE LAS IMAGENES
   obtenerElarrayDeimagenes() {
     this.imagenArray.map((res: any) => {
       this.imageRaton = res.mediana[0];
       this.imgGrande = res.zoom[0];
     });
-  }
+  } //BUSCAR QUE IMAGEN DEL ZOOM SE APARECE DEPENDE DEL INDICE
   buscarImagenZoomdelArray(array: any, i: any) {
     array.map((res: any, index: any) => {
       if (index === i) {
-        this.imgGrande = res;
+        this.imgGrande = res; //ASIGNO LO ENCONTRADO EN LA VARIABLE
       }
     });
   }
+  //BUSCAR QUE IMAGEN DEL ZOOM SE APARECE DEPENDE DEL INDICE
   buscarImagendelArray(array: any, i: any) {
     array.map((res: any, index: any) => {
       if (index === i) {
-        this.imageRaton = res;
+        this.imageRaton = res; //ASIGNO LO ENCONTRADO EN LA VARIABLE
       }
     });
   }
   //PASO POR LA IMAGEN
   mouseCambioImg(i: any) {
     this.imagenArray.map((res: any) => {
-      this.buscarImagendelArray(res.mediana, i);
-      this.buscarImagenZoomdelArray(res.zoom, i);
+      this.buscarImagendelArray(res.mediana, i); //MANDO A BUSCAR
+      this.buscarImagenZoomdelArray(res.zoom, i); //MANDO A BUSCAR
     });
   }
 }
