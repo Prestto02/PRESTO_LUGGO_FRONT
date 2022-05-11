a8f4a045aedd6cb002dad3930d7d0fa779291bc6
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-05-11 09:57:32 -0500

    Se arreglo el bug del offcanvas
    Se arreglo el bug de offcanvas para poder hacer scroll en otros tipos de pantallas

--- src/app/shared/components/index/footer-index/footer/footer.component.ts
+++ src/app/shared/components/index/footer-index/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UrlFront } from 'src/app/shared/routes/RoutesFront';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor(private _route: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  irQuienesSomos() {
+    this._route.navigateByUrl(`${UrlFront.Menu.menu}/${UrlFront.Menu.quienesSomos}`);
   }
-
 }
