0c27cb523f05ded0e23fe6aadc1e3bd24414a87e
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-05-12 14:37:29 -0500

    Responsive detalles productos
    Se mejoro el responsive del detalle del producto.

--- src/app/shared/components/shared-products/detalle-productos/imagen-products/imagen-products.component.ts
+++ src/app/shared/components/shared-products/detalle-productos/imagen-products/imagen-products.component.ts
@@ -11,6 +11,8 @@ export class ImagenProductsComponent implements OnInit {
   imageRaton: string = '';
   imgGrande: string = '';
   enableZoom: boolean = true;
+  styleResults =
+    'background:#fff;width:500px; height:500px; background-repeat: no-repeat; z-index: 2; position:absolute;-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24); top: 0;left: 450px;';
 
   constructor(private ngxImgZoom: NgxImgZoomService) {
     this.ngxImgZoom.setZoomBreakPoints([
