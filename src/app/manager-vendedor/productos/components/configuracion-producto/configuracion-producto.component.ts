a45a61dab08329db2bd70f72aa675ef34d324144
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-05-12 13:11:05 -0500

    Mejorar responsive del index
    Se mejoro el responsive de la pagina principal

--- src/app/manager-vendedor/productos/components/configuracion-producto/configuracion-producto.component.ts
+++ src/app/manager-vendedor/productos/components/configuracion-producto/configuracion-producto.component.ts
@@ -8,10 +8,24 @@ import { BaseFormTamano } from './configuracion-tamano/models/BaseFormTamano';
   styleUrls: ['./configuracion-producto.component.css'],
 })
 export class ConfiguracionProductoComponent implements OnInit {
+  arrayAtributos: any = [
+    {
+      id: 1,
+      nombre: 'Color',
+    },
+    {
+      id: 2,
+      nombre: 'Tamaño',
+    },
+  ];
   constructor(
     public formColor: BaseFormColors,
     public formTamano: BaseFormTamano
   ) {}
 
   ngOnInit(): void {}
+
+  meDistesClick(e: any) {
+    console.log(e);
+  }
 }
