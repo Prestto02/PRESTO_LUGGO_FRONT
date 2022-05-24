60e251c3d575e27e5d704cd8c15f4d196d79d78b
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-05-24 13:55:26 -0500

    retocando el formulario adn

--- src/app/users/components/registrar-adn/models/BaseFormTerminar.ts
+++ src/app/users/components/registrar-adn/models/BaseFormTerminar.ts
@@ -4,20 +4,11 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class BaseFormTerminarAdn {
   constructor(private formB: FormBuilder) {}
   formTerminar = this.formB.group({
-    DondeEres: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(/^[A-Za-z0-9Á-ÿ\u00E0-\u00FC\-_ ]+$/),
-      ],
-    ],
-    FechaNacimiento: ['', [Validators.required]],
+    DondeEres: ['', [Validators.pattern(/^[A-Za-z0-9Á-ÿ\u00E0-\u00FC\-_ ]+$/)]],
+    FechaNacimiento: [''],
     ComoTeDescribes: [
       '',
-      [
-        Validators.required,
-        Validators.pattern(/^[A-Za-z0-9Á-ÿ\u00E0-\u00FC\-_ ]+$/),
-      ],
+      [Validators.pattern(/^[A-Za-z0-9Á-ÿ\u00E0-\u00FC\-_ ]+$/)],
     ],
   });
 }
