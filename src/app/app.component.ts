abf81084ff8c12ad6f2095a056daa3980eb5f0b2
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-06-13 16:45:00 -0500

    Logica para diferentes colores segun la ruta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   //title = 'frontend';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.verifyMenuIndex(event);
+      }
+    });
+  }
+
+  verifyMenuIndex(event: NavigationEnd) {
+    const element: any = <HTMLElement>(
+      document.getElementById('color-container')
+    );
+    if (event.url.match('/Menu/index')) {
+      element.style.background = '#f1f3f6';
+    } else {
+      element.style.background = '#fff';
+    }
+  }
 }
