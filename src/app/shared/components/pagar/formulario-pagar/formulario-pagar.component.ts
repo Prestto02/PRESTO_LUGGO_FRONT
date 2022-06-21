c3d9fcc55cae6ccc642ea4759b42af9203c58a1b
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-06-21 13:53:57 -0500

    Responsive para laptops

--- src/app/shared/components/pagar/formulario-pagar/formulario-pagar.component.ts
+++ src/app/shared/components/pagar/formulario-pagar/formulario-pagar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CiudadPaisService } from 'src/app/users/components/registrar-adn/services/ciudad-pais.service';
 import { BaseFormPagar } from './models/BaseFormPagar';
 
 @Component({
@@ -7,37 +8,18 @@ import { BaseFormPagar } from './models/BaseFormPagar';
   styleUrls: ['./formulario-pagar.component.css'],
 })
 export class FormularioPagarComponent implements OnInit {
-  arrayCodigosPaises: any = [
-    {
-      id: 1,
-      Name: 'Ecuador',
-      Codigo: '+593',
-      icons:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Bandera_Del_Ecuador.jpg/1200px-Bandera_Del_Ecuador.jpg',
-    },
-    {
-      id: 2,
-      Name: 'Colombia',
-      Codigo: '+57',
-      icons:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/1200px-Flag_of_Colombia.svg.png',
-    },
-    {
-      id: 3,
-      Name: 'Argentina',
-      Codigo: '+54',
-      icons:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1200px-Flag_of_Argentina.svg.png',
-    },
-    {
-      id: 4,
-      Name: 'Bolivia',
-      Codigo: '+591',
-      icons:
-        'https://si-educa.net/wp-content/uploads/2013/08/Bandera-Bolivia.jpg',
-    },
-  ];
-  constructor(public formB: BaseFormPagar) {}
+  arrayCodigosPaises: any = [];
+  constructor(
+    public formB: BaseFormPagar,
+    private apiCiudadPais: CiudadPaisService //SERVICES CIUDAD PAISES
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPaisCodigo();
+  }
+  getPaisCodigo() {
+    this.apiCiudadPais.getPaises().subscribe((res) => {
+      this.arrayCodigosPaises = res;
+    });
+  }
 }
