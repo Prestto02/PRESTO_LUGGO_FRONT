c98c41d1615ddbc8e02bb2e0819eb41c2044616a
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-06-22 12:18:41 -0500

    Mejorando responsive

--- src/app/shared/components/index/secciones/section-servicios/section-servicios.component.ts
+++ src/app/shared/components/index/secciones/section-servicios/section-servicios.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { arrayServicios } from './arrayServicios';
 
 @Component({
   selector: 'app-section-servicios',
   templateUrl: './section-servicios.component.html',
-  styleUrls: ['./section-servicios.component.css']
+  styleUrls: ['./section-servicios.component.css'],
 })
 export class SectionServiciosComponent implements OnInit {
+  arrayServicios = arrayServicios;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
