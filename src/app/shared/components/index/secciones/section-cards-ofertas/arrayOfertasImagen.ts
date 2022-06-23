3880a7474be1a61cf08c306c4ce7d106a77711ca
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-06-23 15:37:53 -0500

    Correcion de ofertas

--- src/app/shared/components/index/secciones/section-cards-ofertas/arrayOfertasImagen.ts
+++ src/app/shared/components/index/secciones/section-cards-ofertas/arrayOfertasImagen.ts
@@ -7,7 +7,7 @@ export const arrayOfertasImg = [
     texto: 'TECNOLOGÍA',
     classOfertas: 'card-text text-ofertas text-end',
     classTexto:
-      'card-title text-titulo text-end d-sm-none d-none d-xl-block d-md-block d-xxl-block',
+      'card-title text-titulo text-end d-xl-block d-md-block d-xxl-block',
     imgRuta: `${RepositorioImg.urlRepositorio}img/banners-section-1/sb-tecnologia.jpg`,
   },
   {
@@ -16,7 +16,7 @@ export const arrayOfertasImg = [
     texto: 'AUDÍFONOS',
     classOfertas: 'card-text text-ofertas text-white',
     classTexto:
-      'card-title text-titulo text-white d-sm-none d-none d-xl-block d-md-block d-xxl-block',
+      'card-title text-titulo text-white  d-xl-block d-md-block d-xxl-block',
     imgRuta: `${RepositorioImg.urlRepositorio}img/banners-section-1/sb-audifonos.jpg`,
   },
   {
@@ -34,7 +34,7 @@ export const arrayOfertasImg = [
     texto: 'COSMÉTICOS',
     classOfertas:
       'card-title text-titulo d-sm-none d-none d-xl-block d-md-block d-xxl-block',
-    classTexto: 'card-text text-ofertas text-titulo',
+    classTexto: 'card-title text-titulo  d-xl-block d-md-block d-xxl-block',
     imgRuta: `${RepositorioImg.urlRepositorio}img/banners-section-1/sb-cosmeticos.jpg`,
   },
 ];
