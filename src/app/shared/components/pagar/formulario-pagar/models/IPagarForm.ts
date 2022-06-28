f4dbbdb5f9a87cf866651de443352294a313c6ba
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-06-28 12:47:25 -0500

    Cambio de diseño de errores de inputs

--- src/app/shared/components/pagar/formulario-pagar/models/IPagarForm.ts
+++ src/app/shared/components/pagar/formulario-pagar/models/IPagarForm.ts
@@ -1,6 +1,15 @@
 export interface IFormularioPagar {
   Nombres: string;
   Apellidos: string;
-  Celular: string;
-  Correo: string;
+  cliente_phone: string;
+  code_phone: string;
+  identificacion: string;
+  correo: string;
+  direccion_entrega: string;
+  detalle_Venta: Array<detalle_venta>;
+}
+
+export interface detalle_venta {
+  cantidad: string;
+  Id_detalle_articulo: string;
 }
