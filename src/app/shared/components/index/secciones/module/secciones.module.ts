b8ae8b142cb64f64245e155c933baf89de5cccf2
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-07-12 08:51:36 -0500

    Se quito el componente de responsive

--- src/app/shared/components/index/secciones/module/secciones.module.ts
+++ src/app/shared/components/index/secciones/module/secciones.module.ts
@@ -4,10 +4,8 @@ import { CommonModule } from '@angular/common';
 import { SectionCarouselComponent } from '../section-carousel/section-carousel.component';
 import { SectionNoticiasComponent } from '../section-noticias/section-noticias.component';
 import { SectionNovedadesComponent } from '../section-novedades/section-novedades.component';
-import { ResponsiveSectionNovedadesComponent } from '../section-novedades/responsive-section-novedades/responsive-section-novedades.component';
 import { SectionOfertasComponent } from '../section-ofertas/section-ofertas.component';
 import { SectionOfertas2Component } from '../section-ofertas2/section-ofertas2.component';
-import { ResponsiveSectionOfertas2Component } from '../section-ofertas2/responsive-section-ofertas2/responsive-section-ofertas2.component';
 import { SectionCardsInformationComponent } from '../section-cards-information/section-cards-information.component';
 import { SectionCardsOfertasComponent } from '../section-cards-ofertas/section-cards-ofertas.component';
 import { SectionServiciosComponent } from '../section-servicios/section-servicios.component';
@@ -20,10 +18,8 @@ import { SeccionesComponent } from '../secciones.component';
 
     SectionNoticiasComponent,
     SectionNovedadesComponent,
-    ResponsiveSectionNovedadesComponent, //RESPONSIVE SECTION NOVEDadeS
     SectionOfertasComponent,
     SectionOfertas2Component,
-    ResponsiveSectionOfertas2Component, //RESPONSIVE SECTION OFERTAS 2
     SectionCardsInformationComponent,
     SectionCardsOfertasComponent,
     SectionServiciosComponent,
