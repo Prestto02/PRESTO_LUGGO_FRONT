72c11e9fb7472b377dac2f65ae4fdb9b60419d75
prestto03 <68355110+prestto03@users.noreply.github.com>
2022-08-01 15:38:45 -0500

    Subida a calidad

--- src/app/shared/components/index/menu-index/menu2/directive/identify-users.directive.ts
+++ src/app/shared/components/index/menu-index/menu2/directive/identify-users.directive.ts
@@ -3,22 +3,30 @@ import {
   Directive,
   ElementRef,
   Input,
+  OnInit,
   Renderer2,
+  ViewChild,
   ViewChildren,
 } from '@angular/core';
 
 @Directive({
   selector: '[appIdentifyUsers]',
 })
-export class IdentifyUsersDirective implements AfterViewInit {
+export class IdentifyUsersDirective implements OnInit, AfterViewInit {
   @Input('identyUser') identyUser: string | null = null;
   constructor(private el: ElementRef, private rendered2: Renderer2) {}
-
   ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
     this.userIdentifyCheck();
+    throw new Error('Method not implemented.');
   }
 
   userIdentifyCheck(): void {
-    this.rendered2.parentNode(this.el.nativeElement);
+    const element = <HTMLElement>document.getElementById('miPuertto');
+    const miCuenta = <HTMLElement>document.getElementById('miCuenta');
+    const mensaje = this.rendered2.createText('Mi puertto');
+    this.rendered2.appendChild(element, mensaje);
   }
 }
